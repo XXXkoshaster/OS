e1090feb8abfab2058fe69feb960d227bb91d811
Yan0vich <vorobyovgleb14@gmail.com>
2024-11-05 01:17:02 +0300

    first commit

---
+++ LAB_1/child.c
@@ -0,0 +1,62 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int is_prime(int num) {
+    if (num <= 1)
+        return 0;
+
+    if (num <= 3)
+        return 1;
+
+    if (num % 2 == 0 || num % 3 == 0)
+        return 0;
+		
+    for (int i = 5; i * i <= num; i += 6)
+        if (num % i == 0 || num % (i + 2) == 0)
+            return 0;
+
+    return 1;
+}
+
+int main() {
+    char buf[4096];
+    int num;
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+        char *ptr = buf;
+        while (ptr < buf + bytes_read) {
+            char *endptr;
+            num = strtol(ptr, &endptr, 10);
+
+            if (ptr == endptr) {
+                while (ptr < buf + bytes_read && *ptr != '\n') {
+                    ptr++;
+                }
+                ptr++;
+                continue;
+            }
+
+            if (num < 0 || is_prime(num)) {
+                exit(EXIT_SUCCESS);
+            }
+
+            if (!is_prime(num)) {
+                char output[32];
+                int len = snprintf(output, sizeof(output), "%d\n", num);
+                write(STDOUT_FILENO, output, len);
+            }
+
+            while (ptr < buf + bytes_read && *ptr != '\n') {
+                ptr++;
+            }
+            ptr++;
+        }
+    }
+
+    return 0;
+}
---
+++ build/CMakeFiles/child.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/child.dir/LAB_1/child.c.o"
+  "CMakeFiles/child.dir/LAB_1/child.c.o.d"
+  "child"
+  "child.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/child.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
